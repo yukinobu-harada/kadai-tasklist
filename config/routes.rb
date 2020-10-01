c978b37d4def92b0deae6eeee77d6899c2898f5a
chaki.da-hara <yukiharada2412@gmail.com>
2020-10-01 12:07:26 +0000

    tasklist user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
+  
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:index, :show, :create]
   
   resources :tasks
 end
